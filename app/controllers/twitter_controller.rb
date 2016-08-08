efe9ae7f240e6e221828bb7fb352280f590f0d8e
Thea Amanda <ta.kupler@gmail.com>
2016-08-08 16:58:31 +0200

    only fill followers-variable when not nil

--- app/controllers/twitter_controller.rb
+++ app/controllers/twitter_controller.rb
@@ -1,19 +1,20 @@
 class TwitterController < ApplicationController
-  def test
-      @current_time = Time.now
+  def time
+    #does the view and the method has to have the same name
+  end
+  def sidebar
+    @num_followers = nil
+    @current_time = Time.now
     # only do when twitter_credentials are set as required
     unless Rails.application.secrets.twitter_required == false
-        # have all the tweets in an array
+      if @num_followers == nil
         @num_followers = twitter_client.user.followers_count
-        @tweets = []
-        @twitter_name = twitter_client.user.screen_name
-        # twitter_client.user_timeline.each do |tweet|
-        #   unless tweet.text.start_with?('RT')
-        #     @tweets << tweet.text
-        #   end
-        # end
-          # differ between tweets and retweets! do not show retweets! retweets start with "RT"
-          @tweets = twitter_client.user_timeline.select{ |tweet| tweet.text.start_with?('RT')==false}
+      end
+      # have all the tweets in an array
+      @tweets = []
+      @twitter_name = twitter_client.user.screen_name
+      # differ between tweets and retweets! do not show retweets! retweets start with "RT"
+      @tweets = twitter_client.user_timeline.select{ |tweet| tweet.text.start_with?('RT')==false}
     end
   end
 end
