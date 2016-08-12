3228e587e5fefe410d18b01bd4210a6ca4dd2365
Thea Amanda <ta.kupler@gmail.com>
2016-08-12 12:17:26 +0200

    included caching

--- app/controllers/twitter_controller.rb
+++ app/controllers/twitter_controller.rb
@@ -3,23 +3,38 @@ class TwitterController < ApplicationController
     #does the view and the method has to have the same name
   end
   def sidebar
-    @num_followers = nil
-    @current_time = Time.now
-    # only do when twitter_credentials are set as required
+    num_followers = {}
+    twitter_screen_name = {}
+    twitter_name = {}
     unless Rails.application.secrets.twitter['enabled'] == false
-      # create hash-cache, hash with timestamp; key + value
-      # @myCache = {'Time.now' ==> 'twitter_client.user.followers_count'}
-      # if @num_followers == nil
-      #   @num_followers = twitter_client.user.followers_count
+      # if num_followers.keys[0] == nil
+      #  num_followers = {(Time.now) => twitter_client.user.followers_count}
+      # else
+      #   unless Time.now < num_followers.keys[0] + 15.minutes
+      #     num_followers = {(Time.now) => twitter_client.user.followers_count}
+      #   end
       # end
-      # have all the tweets in an array
-      @tweets = []
-      unless @twitter_screen_name != nil
-        @twitter_screen_name = twitter_client.user.screen_name # how often can this be asked for?
+      # @num_followers = num_followers.values[0]
+
+      if twitter_screen_name.keys[0] == nil
+       twitter_screen_name = {(Time.now) => twitter_client.user.screen_name}
+      else
+        unless Time.now < twitter_screen_name.keys[0] + 15.minutes
+          twitter_screen_name = {(Time.now) => twitter_client.user.screen_name}
+        end
       end
-      unless @twitter_name != nil
-        @twitter_name = twitter_client.user.name # how often can this be asked for?
+      @twitter_screen_name = twitter_screen_name.values[0]
+
+      if twitter_name.keys[0] == nil
+       twitter_name = {(Time.now) => twitter_client.user.name}
+      else
+        unless Time.now < twitter_name.keys[0] + 15.minutes
+          twitter_name = {(Time.now) => twitter_client.user.name}
+        end
       end
+      @twitter_name = twitter_name.values[0]
+
+      # @tweets = []
       # differ between tweets and retweets! do not show retweets! retweets start with "RT"
       @tweets = twitter_client.user_timeline.select{ |tweet| tweet.text.start_with?('RT')==false}
       @tweet_time = "tweeted on" # should say retweeted on for retweet
