0a77426988156880ec803534585381765f52ed97
Thea Amanda <ta.kupler@gmail.com>
2016-08-15 17:58:58 +0200

    updated caching with global variables

--- app/controllers/twitter_controller.rb
+++ app/controllers/twitter_controller.rb
@@ -3,41 +3,30 @@ class TwitterController < ApplicationController
     #does the view and the method has to have the same name
   end
   def sidebar
-    num_followers = {}
-    twitter_screen_name = {}
-    twitter_name = {}
     unless Rails.application.secrets.twitter['enabled'] == false
-      # if num_followers.keys[0] == nil
-      #  num_followers = {(Time.now) => twitter_client.user.followers_count}
-      # else
-      #   unless Time.now < num_followers.keys[0] + 15.minutes
-      #     num_followers = {(Time.now) => twitter_client.user.followers_count}
-      #   end
-      # end
-      # @num_followers = num_followers.values[0]
+      @twitter_screen_name = load_from_cache($twitter_screen_name, "screen_name")
+      @twitter_name = load_from_cache($twitter_name, "name")
 
-      if twitter_screen_name.keys[0] == nil
-       twitter_screen_name = {(Time.now) => twitter_client.user.screen_name}
-      else
-        unless Time.now < twitter_screen_name.keys[0] + 15.minutes
-          twitter_screen_name = {(Time.now) => twitter_client.user.screen_name}
-        end
-      end
-      @twitter_screen_name = twitter_screen_name.values[0]
-
-      if twitter_name.keys[0] == nil
-       twitter_name = {(Time.now) => twitter_client.user.name}
-      else
-        unless Time.now < twitter_name.keys[0] + 15.minutes
-          twitter_name = {(Time.now) => twitter_client.user.name}
-        end
-      end
-      @twitter_name = twitter_name.values[0]
-
-      # @tweets = []
       # differ between tweets and retweets! do not show retweets! retweets start with "RT"
       @tweets = twitter_client.user_timeline.select{ |tweet| tweet.text.start_with?('RT')==false}
       @tweet_time = "tweeted on" # should say retweeted on for retweet
     end
   end
+
+  def load_from_cache(tuple, twitter_user_property)
+    if tuple[0] == nil
+      update_time_based_cache(tuple, twitter_user_property)
+    else
+      unless Time.now < tuple[0] + 15.minutes
+        update_time_based_cache(tuple, twitter_user_property)
+      end
+    end
+    tuple[1]
+  end
+
+  def update_time_based_cache(tuple, twitter_user_property)
+    tuple[0] = Time.now
+    tuple[1] = twitter_client.user[twitter_user_property]
+  end
+
 end
