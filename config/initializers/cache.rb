3ba19a32dfcbe34cb6fac87857636747ac06138b
Thea Amanda <ta.kupler@gmail.com>
2016-08-18 17:53:55 +0200

    update cache

---
+++ config/initializers/cache.rb
@@ -0,0 +1,9 @@
+#get rid of the global variables?
+# def twitter_screen_name
+#   twitter_screen_name  = [nil, nil]
+#   twitter_screen_name
+# end
+
+# define global variables
+$twitter_user_info = [nil, nil]
+# [2016-08-16 22:00:00, {screen_name: 'load_to_code', name: "RGSoC team working ..."}]
